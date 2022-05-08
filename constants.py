dfaffe762a70168e112f8b7eee4221c4fe7df63c
Dodgo1 <91613930+Dodgo1@users.noreply.github.com>
2022-05-08 19:59:57 +0200

    rework

---
+++ constants.py
@@ -0,0 +1,26 @@
+#stałe: koordynaty,nazwy miast, kraje
+Key = "<API KEY HERE>"
+
+# london coordinates
+London = {
+    "COUNTRY" : "England",
+    "LAT" : 51.5073219,
+    "LON" : -0.1276474
+}
+
+# Amsterdam coordinates
+Amsterdam = {
+    "COUNTRY" : "Netherlands",
+    "LAT" : 52,
+    "LON" : 4
+}
+
+# Bombay coordinates
+Bombay = {
+    "COUNTRY" : "India",
+    "LAT" : 19,
+    "LON" : 72.48
+}
+city_names = "Tokyo","Delhi","Dhaka","New York","Mocscow","Paris"
+city_names_incorrect = "tOkYo","DeLhi","dhaKa","new york","MoCscOw","PaRiS"
+
