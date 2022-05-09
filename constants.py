422d54f4261118a5fd251b3f9454fab9f2c9e75f
Adam <a.jaskierski@tkhtechnology.com>
2022-05-09 12:31:16 +0200

    hotfix

---
+++ constants.py
@@ -0,0 +1,25 @@
+# constatnts, city_names, coors etc.
+Key = "<API KEY>"
+
+# london coordinates
+London = {
+    "COUNTRY": "England",
+    "LAT": 51.5073219,
+    "LON": -0.1276474
+}
+
+# Amsterdam coordinates
+Amsterdam = {
+    "COUNTRY": "Netherlands",
+    "LAT": 52,
+    "LON": 4
+}
+
+# Bombay coordinates
+Bombay = {
+    "COUNTRY": "India",
+    "LAT": 19,
+    "LON": 72.48
+}
+city_names = "Tokyo", "Delhi", "Dhaka", "New York", "Moscow", "Paris"
+city_names_incorrect = "tOkYo", "DeLhi", "dhaKa", "new york", "MoCscOw", "PaRiS"
