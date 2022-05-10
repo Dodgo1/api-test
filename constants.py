87b78e855c5aae8301f3682db813714c8665737b
Adam <a.jaskierski@tkhtechnology.com>
2022-05-10 14:34:20 +0200

    new test, json scheme

--- constants.py
+++ constants.py
@@ -1,25 +1,158 @@
 # constatnts, city_names, coors etc.
-Key = "<API KEY>"
+Key = "59e62469a3da06b2a9a30ff73c2c0d05"
 
 # london coordinates
-London = {
+LONDON = {
     "COUNTRY": "England",
     "LAT": 51.5073219,
     "LON": -0.1276474
 }
 
-# Amsterdam coordinates
-Amsterdam = {
+# AMSTERDAM coordinates
+AMSTERDAM = {
     "COUNTRY": "Netherlands",
     "LAT": 52,
     "LON": 4
 }
 
-# Bombay coordinates
-Bombay = {
+# BOMBAY coordinates
+BOMBAY = {
     "COUNTRY": "India",
     "LAT": 19,
     "LON": 72.48
 }
-city_names = "Tokyo", "Delhi", "Dhaka", "New York", "Moscow", "Paris"
-city_names_incorrect = "tOkYo", "DeLhi", "dhaKa", "new york", "MoCscOw", "PaRiS"
+CITY_NAMES = "Tokyo", "Delhi", "Dhaka", "New York", "Moscow", "Paris"
+CITY_NAMES_DIFFERENT_CASES = "tOkYo", "DeLhi", "dhaKa", "new york", "MoCscOw", "PaRiS"
+
+# Json testing
+TEST_RESPONSE = {
+        "base": "stations",
+        "clouds": {
+            "all": 100
+        },
+        "cod": 200,
+        "coord": {
+            "lon": 139,
+            "lat": 35
+        },
+        "dt": 1652173547,
+        "id": 1851632,
+        "main": {
+            "temp": 288.29,
+            "feels_like": 287.66,
+            "temp_min": 288.29,
+            "temp_max": 288.29,
+            "pressure": 1020,
+            "humidity": 69,
+            "sea_level": 1020,
+            "grnd_level": 991
+        },
+        "name": "Shuzenji",
+        "sys": {
+            "country": "JP",
+            "sunrise": 1652125502,
+            "sunset": 1652175351
+        },
+        "timezone": 32400,
+        "visibility": 10000,
+        "weather": [
+            {
+                "id": 804,
+                "main": "Clouds",
+                "description": "overcast clouds",
+                "icon": "04d"
+            }
+        ],
+        "wind": {
+            "speed": 1.92,
+            "deg": 84,
+            "gust": 2.91
+        }
+    }   # response from open weather api for testing purposes
+SCHEMA = {
+    "type": "object",
+    "properties": {
+
+        "base": {
+            "type": "string"
+        },
+
+        "clouds": {
+            "type": "object",
+            "properties": {
+
+                "all": {
+                    "type": "number"
+                }
+            }
+        },
+
+        "cod": {
+            "type": "number"
+        },
+
+        "coord": {
+            "type": "object",
+            "properties": {
+                "lon": {"type": "number"},
+                "lat": {"type": "number"}
+            }
+        },
+
+        "dt": {"type": "number"},
+
+        "id": {"type": "number"},
+
+        "main": {
+            "type": "object",
+            "properties": {
+                "temp": {"type": "number"},
+                "feels_like": {"type": "number"},
+                "temp_min": {"type": "number"},
+                "temp_max": {"type": "number"},
+                "pressure": {"type": "number"},
+                "humidity": {"type": "number"},
+                "sea_level": {"type": "number"},
+                "grnd_level": {"type": "number"}
+            }
+        },
+
+        "name": {"type": "string"},
+
+        "sys": {
+            "type": "object",
+            "properties": {
+                "country": {"type":"string"},
+                "sunrise": {"type":"number"},
+                "sunset": {"type":"number"}
+            }
+        },
+
+        "timezone": {"type": "number"},
+
+        "visibility": {"type": "number"},
+
+        "weather": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "id": {"type": "number"},
+                    "main": {"type": "string"},
+                    "description": {"type": "string"},
+                    "icon": {"type": "string"}
+                }
+            }
+        },
+        "wind": {
+            "type": "object",
+            "properties": {
+                "speed": {"type": "number"},
+                "deg": {"type": "number"},
+                "gust": {"type": "number"}
+            }
+        }
+
+    }
+}           # My Schema full of frustration - it works :D
+
--- test_main.py
+++ test_main.py
@@ -1,53 +1,49 @@
 from http import HTTPStatus
-import requests
+
 import pytest
-from constants import Key, London, Amsterdam, Bombay, city_names, city_names_incorrect
+import requests
+from jsonschema import validate
+
+from constants import Key, LONDON, AMSTERDAM, BOMBAY, CITY_NAMES, CITY_NAMES_DIFFERENT_CASES,SCHEMA
 
 
 # test using lat and lon
+class TestApiCoordinates:
 
-class TestApiCoords:
-
-    def test_london(self):
+    @staticmethod
+    @pytest.mark.parametrize("LAT,LON", [(LONDON["LAT"], LONDON["LON"]), (AMSTERDAM["LAT"], AMSTERDAM["LON"]),
+                                         (BOMBAY["LAT"], BOMBAY["LON"])])
+    def test_coordinates(LAT, LON):
         response = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?lat={London['LAT']}&lon={London['LON']}&appid={Key}")
-        assert response.status_code == HTTPStatus.OK
-
-    def test_amsterdam(self):
-        response = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?lat={Amsterdam['LAT']}&lon={Amsterdam['LON']}&appid={Key}")
-        assert response.status_code == HTTPStatus.OK
-
-    def test_bombay(self):
-        response = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?lat={Bombay['LAT']}&lon={Bombay['LON']}&appid={Key}")
+            f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={Key}")
         assert response.status_code == HTTPStatus.OK
 
 
-class TestApiCity:
+class TestApiCityName:
     # test using city names
 
     @staticmethod
-    @pytest.mark.parametrize("city_name", [city_names[0], city_names[1], city_names[2]])
+    @pytest.mark.parametrize("city_name", [CITY_NAMES[0], CITY_NAMES[1], CITY_NAMES[2]])
     def test_city(city_name):
         response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={Key}")
         assert response.status_code == HTTPStatus.OK
 
     @staticmethod
-    @pytest.mark.parametrize("city_name", [city_names_incorrect[0], city_names_incorrect[1], city_names_incorrect[2]])
-    def test_city_incorrect(city_name):
+    @pytest.mark.parametrize("city_name", [CITY_NAMES_DIFFERENT_CASES[0], CITY_NAMES_DIFFERENT_CASES[1],
+                                           CITY_NAMES_DIFFERENT_CASES[2]])
+    def test_city_cases(city_name):
         response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={Key}")
         assert response.status_code == HTTPStatus.OK
 
 
 class TestApiError:
 
-    def test_coords_letter(self):
+    def test_coordinates_letter(self):
         response = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?lat={'a'}&lon={'b'}&appid={Key}")
         assert response.status_code == HTTPStatus.BAD_REQUEST
 
-    def test_coords_number(self):
+    def test_coordinates_number(self):
         response = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?lat={'d'}&lon={'f'}&appid={Key}")
         assert response.status_code == HTTPStatus.BAD_REQUEST
@@ -61,11 +57,11 @@ class TestApiError:
         assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-class TestApiErrorParam:
+class TestApiErrorParametrized:
 
     @staticmethod
     @pytest.mark.parametrize("lat,lon", [("a", "b"), ("c", "d")])
-    def test_coords(lat, lon):
+    def test_coordinates(lat, lon):
         response = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={Key}")
         assert response.status_code == HTTPStatus.BAD_REQUEST
@@ -75,4 +71,19 @@ class TestApiErrorParam:
     def test_city(city_name):
         response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={Key}")
         assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+class TestApiResponse:
+
+    response = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?lat={'10'}&lon={'25'}&appid={Key}")
+
+    @pytest.mark.parametrize("LAT,LON", [(LONDON["LAT"], LONDON["LON"]), (AMSTERDAM["LAT"], AMSTERDAM["LON"]),
+                                         (BOMBAY["LAT"], BOMBAY["LON"])])
+    def test_response_types(self,LAT, LON):
+        validate(instance=self.response.json(), schema=SCHEMA)
+
+    def test_if_response_exists(self):
+
+
 
